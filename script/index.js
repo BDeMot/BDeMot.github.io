7ca598e7b85b6fba9175063b1de5b9b629e50249
Benjamin De Mot <benjamindm@hotmail.fr>
2021-08-18 21:29:29 +0200

    first version

--- script/index.js
+++ script/index.js
@@ -5,16 +5,80 @@ const scroll = new LocomotiveScroll({
   smartphone: { smooth: true }
 });
 
-scroll.on('call', (args) => {
 
-switch(args) {
-  case 'bigLettersTurnWhiteTrigger': 
-  turnToWhite( document.getElementById('bigLetters') )
-}
+// ********** Workaround to fix the scroll container size bug with Locomotive Scroll
+
+setTimeout(
+  function(){ 
+    scroll.update();
+  }, 
+  100);
+
+scroll.on('call', (args) => {
+  switch(args) {
+    case 'bigLettersTurnWhiteTrigger': 
+    turnToWhite( document.getElementById('bigLetters') )
+    makeElementsAppear()
+    // scroll.update()
+  }
 })
 
 const turnToWhite = el => {
-  return el.style.color = 'white';
+  return el.style.color === 'white' ?  el.style.color = 'rgb(31, 30, 30)' : el.style.color = 'white'
 }
 
 
+const makeElementsAppear = () => {
+  document.getElementById('profilPic').style.opacity = '1'
+  document.getElementById('nav').style.opacity = '1'
+}
+
+  
+
+// ********** Projects List BG color change on hover
+
+const elements = document.getElementsByClassName('projects')
+const elements__vids = document.getElementsByClassName('preview')
+const arrayOfElements = [...elements]
+
+const randomColor = () => {
+  let colors = ['#3FB984', '#F0DB4F', '#2196F3', '#FF5722', '#3A304A']
+  return colors[Math.floor((Math.random() * colors.length))]
+}
+
+arrayOfElements.forEach(el =>{ 
+  el.addEventListener('mouseenter', () => {
+  el.style.backgroundColor = randomColor()
+  el.nextElementSibling.style.backgroundColor = el.style.backgroundColor
+
+  }),
+  el.addEventListener('mouseleave', () => {
+    el.style.backgroundColor = 'rgb(31, 30, 30)'
+  })
+})
+
+
+//********** Prevent default links' behavior and add 'scrollTo' methods
+
+const autoScroller = (start, target) => {
+  const scrollTo_trigger = [...document.getElementsByClassName(start)]
+  scrollTo_trigger.forEach(el => {
+    el.addEventListener('click', (e) => {
+      e.preventDefault()
+      scroll.scrollTo(target)
+    })
+  })
+}
+
+const nav_scrollTo = [
+  {start: 'trigger_scrollToAboutMe',
+  target: '#aboutMe'},
+  {start: 'trigger_scrollToProjects',
+  target: '#projects'},
+  {start: 'trigger_scrollToContacts',
+  target: '#contact'}
+]
+
+nav_scrollTo.forEach(el => {
+  autoScroller(el.start, el.target)
+} )
